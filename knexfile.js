afcf5ffbcf075aaef82f7675394f33d5be51ad7b
Leta Keane <letakeane+turing@gmail.com>
2017-07-13 23:17:23 -0600

    Add yml configure file

--- knexfile.js
+++ knexfile.js
@@ -29,7 +29,7 @@ module.exports = {
       directory: './db/migrations'
     },
     seeds: {
-      directory: './db/seeds/production'
+      directory: './db/seeds/dev'
     },
     useNullAsDefault: true
   },
@@ -41,7 +41,7 @@ module.exports = {
       directory: './db/migrations'
     },
     seeds: {
-      directory: './db/seeds/production'
+      directory: './db/seeds/dev'
     },
     useNullAsDefault: true
   }
